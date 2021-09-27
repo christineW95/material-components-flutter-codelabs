f38289a747905265a37522405a64f32c30542425
Christina William <39514882+christineW95@users.noreply.github.com>
2021-09-27 16:58:37 +0200

    add theme

--- mdc_100_series/lib/app.dart
+++ mdc_100_series/lib/app.dart
@@ -16,7 +16,24 @@ import 'package:flutter/material.dart';
 
 import 'home.dart';
 import 'login.dart';
+import 'color.dart';
 
+final ThemeData _kShrineTheme = _buildShrineTheme();
+
+ThemeData _buildShrineTheme() {
+  final ThemeData base = ThemeData.light();
+  return base.copyWith(
+    colorScheme: base.colorScheme.copyWith(
+      primary: kShrinePink100,
+      onPrimary: kShrineBrown900,
+      secondary: kShrineBrown900,
+      error: kShrineErrorRed,
+    ),
+    // TODO: Add the text themes (103)
+    // TODO: Add the icon themes (103)
+    // TODO: Decorate the inputs (103)
+  );
+}
 // TODO: Convert ShrineApp to stateful widget (104)
 class ShrineApp extends StatelessWidget {
   const ShrineApp({Key? key}) : super(key: key);
@@ -25,6 +42,7 @@ class ShrineApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Shrine',
+
       // TODO: Change home: to a Backdrop with a HomePage frontLayer (104)
       home: const HomePage(),
       // TODO: Make currentCategory field take _currentCategory (104)
@@ -33,6 +51,7 @@ class ShrineApp extends StatelessWidget {
       initialRoute: '/login',
       onGenerateRoute: _getRoute,
       // TODO: Add a theme (103)
+      theme: _kShrineTheme,
     );
   }
 
